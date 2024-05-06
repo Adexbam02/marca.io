ff82bfef39fed1c2fe91fae70ce0a15070980bf7
Nurain <nurainbamidelejnr@gmail.com>
2024-05-06 06:49:25 +0100

    logosba revisit

--- src/components/LogosBar.tsx
+++ src/components/LogosBar.tsx
@@ -3,7 +3,7 @@ import { logos } from "../data";
 
 export default function LogosBar() {
   return (
-    <div className="px-[4rem] md:px-[9rem] flex gap-7 flex-wrap items-center justify-center md:justify-between py-[2rem] md:py-[2rem]">
+    <div className="px-[1rem] md:px-[9rem] py-[2rem] md:py-[2rem] mt-8 flex gap-7 flex-wrap items-center justify-center md:justify-between">
       {logos.map(({ id, src }) => (
         <Image
           key={id}
