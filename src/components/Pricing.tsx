34cb6192aee1eba1bce6138a1b9eb079860603ef
Nurain <nurainbamidelejnr@gmail.com>
2024-05-06 07:05:03 +0100

    Pricing revisited

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,16 +1,22 @@
 import Image from "next/image";
-import { exo, poppins } from "../ui/font";
+import { duru_Sans, exo, poppins } from "../ui/font";
 import Button from "../ui/Button";
 import PricingCard from "../ui/PricingCard";
 
 export default function Pricing() {
   return (
-    <section className="px-[4rem] md:px-[9rem] flex flex-col items-center justify-center gap-3">
+    <section className="px-[1rem] md:px-[9rem] flex flex-col items-center justify-center gap-3">
       <div className="flex flex-col items-center justify-center gap-3 text-center mb-8">
-        <h1 className={`${exo.className} text-cl font-extrabold text-[35px] `}>
+        <h1
+          className={`${exo.className} text-cl text-[32px] md:text-[52px] md:tracking-tighter	font-extrabold`}
+        >
           Pricing
         </h1>
-        <p className="max-w-[80%]">In virtual space through communication platforms. </p>
+        <p
+          className={`${duru_Sans.className} text-[12px] md:text-[15px] md:w-[80%] text-indigo-900 `}
+        >
+          In virtual space through communication platforms.{" "}
+        </p>
       </div>
 
       <div className="flex flex-col items-center justify-center md:flex-row md:gap-3">
@@ -20,7 +26,7 @@ export default function Pricing() {
           color="bg-white"
           textColor=""
           priceValText="text-indigo-900"
-          padding="p-[30px]"
+          padding="p-[35px]"
         />
         <PricingCard
           who={`For Teams`}
@@ -28,7 +34,7 @@ export default function Pricing() {
           color="bg-purple-950"
           textColor="text-white"
           priceValText="text-white"
-          padding="p-[35px]"
+          padding="p-[45px]"
         />
         <PricingCard
           who={`For Company`}
@@ -36,7 +42,7 @@ export default function Pricing() {
           color="bg-white"
           textColor=""
           priceValText="text-indigo-900"
-          padding="p-[30px]"
+          padding="p-[35px]"
         />
       </div>
     </section>
