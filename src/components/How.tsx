86d67fa3d00e293fd9e71e7d19105939cfbadcaf
Nurain <nurainbamidelejnr@gmail.com>
2024-05-06 07:02:08 +0100

    How revisited

--- src/components/How.tsx
+++ src/components/How.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { exo } from "../ui/font";
+import { duru_Sans, exo } from "../ui/font";
 import HowOne from "../ui/HowOne";
 import HowTwo from "../ui/HowTwo";
 import HowThree from "../ui/HowThree";
@@ -8,17 +8,17 @@ export default function How() {
   const colT = `[column-fill:_balance] sm:columns-2 sm:gap-6 lg:columns-1 lg:gap-8`;
   return (
     <section
-      className={`px-[4rem] md:px-[9rem] pt-14 py-[0rem] md:py-[1rem] flex items-center justify-center flex-col gap-10 md:gap-4 md:grid grid-rows-2 grid-flow-col md:items-start`}
+      className={`px-[1rem] md:px-[9rem] pt-14 py-[0rem] md:py-[1rem] flex items-center justify-center flex-col gap-10 md:gap-4 md:grid grid-rows-2 grid-flow-col md:items-start`}
     >
     {/* // <section className={`${colT} px-[4rem] md:px-[9rem] pt-14 py-[0rem] md:py-[1rem]`}> */}
       <div>
-        <div className="flex items-center md:items-start justify-center flex-col">
+        <div className="flex items-center md:items-start justify-center flex-col text-center">
           <h1
-            className={`${exo.className} text-cl font-bold text-[35px] md:text-[45px]`}
+            className={`${exo.className} text-cl text-[32px] md:text-[52px] md:tracking-tighter	font-extrabold`}
           >
             How it Works?
           </h1>
-          <p className="w-[100%] md:w-[400px] text-center md:text-left text-indigo-900">
+          <p className={`${duru_Sans.className} text-[12px] md:text-[15px] md:w-[80%] text-indigo-900 `}>
             Resolving neglected sir tolerably but existence conveying for. Day
             his put off unaffected literature partiality inhabiting.
           </p>
