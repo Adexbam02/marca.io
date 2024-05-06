217c70c68c2bece05d6bda9a6b5bab3b3a92a8ea
Nurain <nurainbamidelejnr@gmail.com>
2024-05-06 06:58:12 +0100

    Offer revisited

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#FCFCFF] relative `}>
+      <body className={`${inter.className} bg-[#FCFCFF] relative overflow-x-hidden`}>
         <Navbar />
         {children}
         <Footer />
--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import { offer } from "../data";
-import { duru_Sans, poppins } from "../ui/font";
+import { duru_Sans, exo, poppins } from "../ui/font";
 
 export default function Offer() {
   return (
-    <section className="px-[4rem] md:px-[9rem] flex flex-col items-center justify-center py-[0rem]  md:py-[1rem]">
+    <section className="px-[1rem] md:px-[9rem] flex flex-col items-center justify-center py-[0rem]  md:py-[1rem]">
       <div className="flex flex-col text-center justify-center items-center">
         <h1
-          className={`${poppins.className} text-cl text-[32px] md:text-[52px] md:tracking-tighter	 head-txt`}
+          className={`${exo.className} text-cl text-[32px] md:text-[52px] md:tracking-tighter	font-extrabold`}
         >
           What we Offer?
         </h1>
@@ -21,17 +21,17 @@ export default function Offer() {
       <div className="grid grid-rows-6 sm:grid-rows-3 md:grid-rows-2 grid-flow-col md:gap-4 p-4 ">
         {offer.map(({ id, src, title, content }) => (
           <div key={id} className="p-2">
-            <div className="py-4 bg-white px-5 flex flex-col gap-5 items-start justify-between bg-hite rounded-[10px] drop-shadow-md">
-              <span className="w-[40px] h-[40px] bg-indigo-50 rounded-[40px] flex items-center justify-center ">
+            <div className="py-[3rem] px-[2rem] bg-white flex flex-col gap-3 items-start justify-between bg-hite rounded-[10px] drop-shadow-md">
+              <span className="w-[50px] h-[50px] bg-indigo-50 rounded-[40px] flex items-center justify-center ">
                 <Image src={src} width={30} height={30} alt="" className="" />
               </span>
               <p
-                className={`${poppins.className} text-indigo-900 text-[15px] font-bold`}
+                className={`${poppins.className} text-indigo-900 text-[25px] font-bold`}
               >
                 {title}
               </p>
               <p
-                className={`${poppins.className} h-[70px] w-[160px] p-1 text-indigo-900 text-[10px] max-w-[150px] leading-snug`}
+                className={`${poppins.className} h-[70px] max-w-[90%] p-1 text-indigo-900 text-[15px]  leading-snug`}
               >
                 {content}
               </p>
