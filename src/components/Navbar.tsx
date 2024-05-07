d7d3305044454ed29e1db792698c600fa4482fec
Nurain <nurainbamidelejnr@gmail.com>
2024-05-07 11:39:17 +0100

    fixing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
   };
 
   return (
-    <Disclosure as="nav" className="bg-gry-800 fixed w-full">
+    <Disclosure as="nav" className="bg-gry-800 fixed w-full z-50 mb-7">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
